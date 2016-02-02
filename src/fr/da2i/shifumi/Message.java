2ac6d4e556eb917ab15c2a19de8c6d1c0acb60e4
Edouard CATTEZ <edouard.cattez@sfr.fr>
2016-02-02 16:55:53 +0100

    Préparation partie 2

--- src/fr/da2i/shifumi/Message.java
+++ src/fr/da2i/shifumi/Message.java
@@ -3,36 +3,55 @@ package fr.da2i.shifumi;
 public class Message {
 	
 	public enum Status {
-		JOIN, QUIT, DO, WAIT, READY, END_ROUND, END_GAME, STOP, ERROR;
+		UNDEF, JOIN, QUIT, DO, WAIT, READY, END_ROUND, END_GAME, STOP, ERROR;
 	}
 	
-	private String status;
+	private Status status;
 	private String data;
 	private String option;
 	
 	public Message(Status status, String data, String option) {
-		this(status.toString(), data, option);
+		this.status = status;
+		this.data = data;
+		this.option = option;
 	}
 	
 	public Message(Status status, String data) {
 		this(status, data, null);
 	}
 	
-	public Message(String status, String data, String option) {
-		this.status = status;
-		this.data = data;
-		this.option = option;
-	}
-	
-	public Message(String status, String data) {
-		this(status, data, null);
-	}
-	
 	public Message() {}
 	
+	public Status getStatus() {
+		if (status == null) {
+			status = Status.UNDEF;
+		}
+		return status;
+	}
+
+	public void setStatus(Status status) {
+		this.status = status;
+	}
+
+	public String getData() {
+		return data;
+	}
+
+	public void setData(String data) {
+		this.data = data;
+	}
+
+	public String getOption() {
+		return option;
+	}
+
+	public void setOption(String option) {
+		this.option = option;
+	}
+
 	@Override
 	public String toString() {
-		String str = status;
+		String str = status.toString();
 		if (data != null) {
 			str += ":" + data;
 		}
@@ -40,6 +59,25 @@ public class Message {
 			str += ":" + option;
 		}
 		return str;
+	}
+	
+	public static Message from(String str) {
+		Message msg = new Message();
+		if (str.contains(":")) {
+			String[] sp = str.split(":");
+			try {
+				if (sp.length >= 1) {
+					msg.setStatus(Status.valueOf(sp[0]));
+				}
+				if (sp.length >= 2) {
+					msg.setData(sp[1]);
+				}
+				if (sp.length == 3) {
+					msg.setOption(sp[2]);
+				}
+			} catch (Exception e) {}
+		}
+		return msg;
 	}
 
 }
--- src/fr/da2i/shifumi/ServeurUDP.java
+++ src/fr/da2i/shifumi/ServeurUDP.java
@@ -4,33 +4,59 @@ import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.util.ArrayList;
+import java.util.List;
+
+import fr.da2i.shifumi.Message.Status;
 
 public class ServeurUDP {
 	
 	private final static int _dgLength = 50;
 	
-	private String p1;
-	private String p2;
+	private List<String> players;
 	
 	private DatagramSocket dgSocket;
 	private DatagramPacket dgPacket;
-	private int _udpPort;
 	
 	public ServeurUDP(int _udpPort) throws IOException {
-		this._udpPort = _udpPort;
 		dgSocket = new DatagramSocket(_udpPort);
+		players = new ArrayList<>();
 	}
 
 	private void go() throws IOException {
+		Message toSend = new Message();
 		InetAddress address;
 		int port;
 		while (true) {
 			// Attente de réception d'un datagramme
-			String msg = receive();
-			msg = msg + "World !";
+			String received = receive();
 			// 	Récupération de l'adresse et du port du client
 			address = dgPacket.getAddress();
 			port = dgPacket.getPort();
+			
+			Message msg = Message.from(received);
+			Status status = msg.getStatus();
+			
+			if (status == Status.UNDEF) {
+				toSend.setStatus(Status.ERROR);
+				toSend.setData("Bad request");
+			}
+			else {
+				if (!players.contains(msg.getData())) {
+					toSend.setStatus(Status.ERROR);
+					toSend.setData("Not in game");
+				}
+				else if (status == Status.JOIN) {
+					
+				}
+				else if (status == Status.QUIT) {
+					
+				}
+				else if (status == Status.DO) {
+					
+				}
+			}
+			
 			send(msg, address, port);
 		}
 	}
@@ -40,6 +66,10 @@ public class ServeurUDP {
 		dgPacket = new DatagramPacket(buffer, _dgLength);
 		dgSocket.receive(dgPacket);
 		return new String(dgPacket.getData(), dgPacket.getOffset(), dgPacket.getLength());
+	}
+	
+	private void send(Message msg, InetAddress address, int port) throws IOException {
+		send(msg.toString(), address, port);
 	}
 	
 	private void send(String msg, InetAddress address, int port) throws IOException {
